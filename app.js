6a5a930cd654e2dd7bc9fe805d396418550b3f92
scriptMang <aperalta686@gmail.com>
2026-01-08 10:28:22 -0500

    Remove string idx assignment, js strings can't be changed

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 function formatFullname (firstName, lastName) {
     let a = firstName[0];
     let b = lastName[0];
-    firstName[0] = a.toUpperCase();
-    lastName[0] = b.toUpperCase();
     console.log(`${lastName}, ${firstName}`);
 }
 
