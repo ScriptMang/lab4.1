a102dbef9dd175d9607d2aa0504b60bcbcae2189
scriptMang <aperalta686@gmail.com>
2026-01-12 01:03:25 -0500

    refactor func calculate-total-cost with discount parameter

--- app.js
+++ app.js
@@ -11,10 +11,13 @@ function formatFullname (firstName, lastName) {
 }
 
 // Task2, Task4
-function calculateTotalCost(price, quantity, taxRate) {
+function calculateTotalCost(price, quantity, taxRate, discount) {
     if (typeof price !== "number" || 
         typeof quantity !== "number" || typeof taxRate !== "number") {
              return "\"Invalid input\"";
+    }
+    if (discount !== undefined) {
+        return totalCost = ((price * quantity) - discount) * (1 + taxRate);
     }
     return totalCost = (price * quantity) * (1 + taxRate);
 }
@@ -40,6 +43,7 @@ function checkEligibility(age, isEmployed) {
 formatFullname("john", "stickman");
 console.log(`Total cost of 5 plates at $12 with 7% tax-rate: ` + calculateTotalCost(12, 5, 0.07));
 console.log(`Total cost of 5 plates at $12 with 7% tax-rate: ` + calculateTotalCost("x", 5, 0.07));
+console.log(`Total cost of 5 plates at $12 with 7% tax-rate.\nApplied discount of $12: ` + calculateTotalCost(13, 5, 0.07, 12));
 console.log(`Bobby is 23 and employed: ` + checkEligibility(23, "employed"));
 console.log(`Ren is 28 and unemployed: ` + checkEligibility(28, "unemployed"));
 console.log(`Jimmy is 14 and unemployed: ` + checkEligibility(14, "unemployed"));
